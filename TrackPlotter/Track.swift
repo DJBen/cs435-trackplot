741f5858727a6dbbbee4b0d72daf57cd032248c0
DJBen <lsh32768@gmail.com>
2015-05-04 06:29:38 -0400

    first commit

---
+++ TrackPlotter/Track.swift
@@ -0,0 +1,13 @@
+//
+//  Track.swift
+//  TrackPlotter
+//
+//  Created by Sihao Lu on 5/4/15.
+//  Copyright (c) 2015 Sihao Lu. All rights reserved.
+//
+
+import UIKit
+
+class Track: NSObject {
+   
+}
